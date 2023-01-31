742e304efa022137616c4a65e7b15616b74f7e9c
RamuK2001 <85925640+RamuK2001@users.noreply.github.com>
2023-01-31 11:51:26 +0530

    Update: README.md azure-pipelines.yml notebooks

---
+++ notebooks/DBNotebook.py
@@ -0,0 +1,21 @@
+# Databricks notebook source
+dbutils.widgets.text("writeMode","overwrite")
+
+# COMMAND ----------
+
+writeMode = dbutils.widgets.get("writeMode")
+
+# COMMAND ----------
+
+if(writeMode == "append"):
+    result = dbutils.notebook.run("AppendNotebook",60)
+elif(writeMode == "overwrite"):
+    result = dbutils.notebook.run("OverwriteNotebook",60)
+elif(writeMode == "delete"):
+    result = dbutils.notebook.run("DeleteNotebook",60)
+else:
+    dbutils.notebook.exit("Wrong input parameter")
+
+# COMMAND ----------
+
+print(result)
